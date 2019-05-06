8119709de78aa46cf488c10b12ac29a24f653c39
ATEZ <shieldnet64@gmail.com>
2019-05-06 19:44:30 +0900

    Simple MQTT Processor
     * Listen MQTT Message from topic 'topic'
     * Subscribe/Publish to topic 'topic'
     * TODO : Add Setting File for not uploading IP address and port and topic information

---
+++ mqtt_processor/listen_and_getfile.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result coe " + str(rc))
+    client.subscribe("topic")
+
+def on_message(client, userdata, msg):
+    print("Topic: " + msg.topic + "\nMessage" + str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("52.194.252.52", 1883, 60)
+client.loop_forever()
