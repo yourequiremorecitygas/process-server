c9215227e730e4a8a9d164905ed75115d84af73f
ATEZ <shieldnet64@gmail.com>
2019-06-05 02:40:20 +0900

    Update bin_to_png.py

--- bin_to_png.py
+++ bin_to_png.py
@@ -7,12 +7,4 @@ if len(sys.argv) is not 2:
 
 file_name = "./"+sys.argv[1]
 raw_file_name = file_name[:-4]+".raw"
-"""
-arr = open(file_name, "r").read().split(",")
-arr = list(map(int, arr))
-f = open(raw_file_name[:-4], "wb")
-newByteArr = bytearray(arr)
-f.write(newByteArr)
-f.close()
-"""
 os.system('./convert -f '+sys.argv[1])
